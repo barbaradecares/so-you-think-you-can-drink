f56e3bc42d19a97a496d972eb45e1440b2a2a7cd
barbaradecares <barbaradecares@hotmail.com>
2018-11-12 17:55:04 -0600

    migrations final update tks god byeee

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -1,4 +1,5 @@
 class Turn < ApplicationRecord
-  belongs_to :player
-  belongs_to :challenge
+   belongs_to :challenge
+   belongs_to :player1, foreign_key: :player1_id, class_name: "Player"
+   belongs_to :player2, foreign_key: :player2_id, class_name: "Player"
 end
--- db/migrate/20181112183542_create_turns.rb
+++ db/migrate/20181112183542_create_turns.rb
@@ -1,8 +1,8 @@
 class CreateTurns < ActiveRecord::Migration[5.2]
   def change
     create_table :turns do |t|
-      t.belongs_to :player1, :class_name => “Player”, :foreign_key => player1_id
-      t.belongs_to :player2, :class_name => “Player”, :foreign_key => player2_id
+      t.integer :player1_id
+      t.integer :player2_id
       t.belongs_to :challenge, foreign_key: true
       t.integer :winner_id
 
