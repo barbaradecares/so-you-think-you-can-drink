08ce2b5d27d1002ed0b30575499b15ab9c52866a
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-11-13 17:46:30 -0600

    create challenge add

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,2 +1,19 @@
 class ChallengesController < ApplicationController
+    def new
+        @challenges = Challenge.all
+        @challenge = Challenge.new
+    end
+
+    def index
+        @challenges = Challenge.all
+    end
+
+    def create
+        Challenge.create(challenge_params)
+        redirect_to new_challenge_path
+    end
+
+    def challenge_params
+        params.require(:challenge).permit(:video_url, :drinks, :title, :description)
+    end
 end
